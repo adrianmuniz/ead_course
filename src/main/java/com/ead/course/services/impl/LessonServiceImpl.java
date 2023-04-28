bf95841bd1c2b52fd14429296b3b32eb9648e264
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2023-04-28 19:35:21 -0300

    Filtros Avançados com Relacionamento no Lesson

--- src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -3,6 +3,9 @@ package com.ead.course.services.impl;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.services.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.ead.course.models.LessonModel;
@@ -35,5 +38,10 @@ public class LessonServiceImpl implements LessonService {
     @Override
     public List<LessonModel> findAllByModule(UUID moduleId) {
         return lessonRepository.findAllLessonsIntoModule(moduleId);
+    }
+
+    @Override
+    public Page<LessonModel> findAllByModule(Specification<LessonModel> spec, Pageable pageable) {
+        return lessonRepository.findAll(spec, pageable);
     }
 }
--- src/main/java/com/ead/course/specifications/SpecificationTemplate.java
+++ src/main/java/com/ead/course/specifications/SpecificationTemplate.java
@@ -39,4 +39,14 @@ public class SpecificationTemplate {
             return cb.and(cb.equal(course.get("courseId"), courseId), cb.isMember(module, coursesModules));
         };
     }
+
+    public static Specification<LessonModel> lessonModuleId(final UUID moduleId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Root<LessonModel> lesson = root;
+            Root<ModuleModel> module = query.from(ModuleModel.class);
+            Expression<Collection<LessonModel>> moduleLesson = module.get("lessons");
+            return cb.and(cb.equal(module.get("moduleId"), moduleId), cb.isMember(lesson, moduleLesson));
+        };
+    }
 }
